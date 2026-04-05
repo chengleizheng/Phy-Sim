b3c5224954861d3b786c7aacde43b8a421e8f419
Channy <1299020141@qq.com>
2026-04-05 12:03:16 +0800

    complete ForceManager.h in Common.

---
+++ src/VCX/Labs/Common/ForceManager.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Engine/Camera.hpp"
+#include "Engine/math.hpp"
+#include "Labs/Common/ImGuiHelper.h"
+
+namespace VCX::Labs::Common {
+    class ForceManager {
+        public:
+            ForceManager() = default;
+
+            bool isEnabled = true;
+            float ForceScale = 1.0f; 
+
+            void ProcessInput(Engine::Camera & camera, ImVec2 const & mousePos);
+            glm::vec3 getForce();   //just force vector
+            std::pair<glm::vec3,glm::vec3> getForce(glm::vec3 cubeCenter);   // force with the point of application
+            std::pair<glm::vec3,glm::vec3> getForce(std::vector<glm::vec3> candidatePoints);    //apply to the closest point among candidate points
+        
+        private:
+            glm::vec3 _deltaForce = glm::vec3 { 0.f };
+            Engine::Camera _camera;
+            glm::vec3 _mouseDirection = glm::vec3 { 0.f };
+        };
+}// namespace VCX::Labs::Common
